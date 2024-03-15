bc78fcba799f2cd314e5b5110d4b163f7e02b558
pavithran_paviii <pavithranr65@gmail.com>
2024-03-16 01:56:55 +0530

    CC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 
 //components
-import Construction from "./components/construction";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Construction /> */}
       <Home />
     </div>
   );
